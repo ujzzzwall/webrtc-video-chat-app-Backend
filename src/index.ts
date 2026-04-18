25440376c23c1f26fcda31aa485b708a078a0d48
Ujjwal Singh <ujzzzwall@gmail.com>
2026-04-18 20:59:08 +0530

    Added basic socket setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import express from 'express';
 import http from 'http';
 import serverConfig from './config/server-config'
+import { Server } from 'socket.io';
+import cors from 'cors';
+
 
 
 const app = express();
+app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin : "*",
+        methods:["GET","POST"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log("new user connected");
+    
+    socket.on("disconnect",()=>{
+        console.log("User disconnected")
+    })
+})
 
 server.listen( serverConfig.PORT ,()=>{
     console.log(`server is up at PORT : ${serverConfig.PORT}`)
